156a8de2de608d710e84f438218c81b3284e7630
codeacme17 <vir_kings@qq.com>
2023-10-29 14:54:29 +0800

    feat: implete mark current feature

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,8 @@ import browser from 'webextension-polyfill'
 import { useState } from 'react'
 import { useTheme } from '@/components/theme-provider'
 import { useStoragedDataList } from '@/lib/hooks/use-storaged-data-list'
+import { addDataToStrageList } from '@/lib/handle-storage'
+import { ListDataItem } from '@/components/mark-table'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -48,6 +50,23 @@ export const Navbar = () => {
     setStoragedDataList(temp)
   }
 
+  const handleMarkCurrentWeb = async () => {
+    const tabInfo = (
+      await browser.tabs.query({ active: true, currentWindow: true })
+    )[0]
+
+    const data: ListDataItem = {
+      label: tabInfo.title!,
+      src: tabInfo.url!,
+      srcLabel: tabInfo.url!,
+      iconUrl: tabInfo.favIconUrl! || '',
+      priority: 1,
+    }
+
+    await addDataToStrageList(data, tabInfo)
+    window.close()
+  }
+
   return (
     <nav className="fixed top-0 w-full p-3 pb-0 z-20 backdrop-blur-md">
       <div className="flex h-full justify-between items-center border-b pb-3 border-solid border-primary/10">
@@ -59,7 +78,12 @@ export const Navbar = () => {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" variant="ghost" className="w-6 h-6 mr-2">
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="w-6 h-6 mr-2"
+                  onClick={handleMarkCurrentWeb}
+                >
                   <Plus className="w-4 h-4" />
                 </Button>
               </TooltipTrigger>
